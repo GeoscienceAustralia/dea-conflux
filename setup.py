7e8b68cd21cd3cb333fb680ad0c129e4a50faa4f
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-08-10 15:51:05 +1000

    added click to install requires

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
         name='dea-conflux',
         packages=['dea_conflux'],
         install_requires=[
+            'click',
         ],
         entry_points = {
             'console_scripts': ['dea-conflux=dea_conflux.__main__:main'],
