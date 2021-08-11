823743d72af35d74d7990d13e1926bf862f75648
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-08-11 13:21:25 +1000

    Added test data for plugins

---
+++ tests/data/sum_wet.conflux.py
@@ -0,0 +1,12 @@
+product_name = 'sum_wet'
+version = '0.0.1'
+
+input_products = {
+    'ga_ls_wo_3': ['water'],
+}
+
+def transform(inputs: xr.Dataset) -> xr.Dataset:
+    return inputs == 128
+
+def summarise(inputs: xr.Dataset) -> xr.Dataset:
+    return inputs.water.sum()
---
+++ tests/data/sum_wet_missing_transform.conflux.py
@@ -0,0 +1,9 @@
+product_name = 'sum_wet'
+version = '0.0.1'
+
+input_products = {
+    'ga_ls_wo_3': ['water'],
+}
+
+def summarise(inputs: xr.Dataset) -> xr.Dataset:
+    return inputs.water.sum()
