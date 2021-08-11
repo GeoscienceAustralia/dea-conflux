6658411aef357fbd3c78643a651e9b10c2d370e5
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-08-11 16:23:23 +1000

    Added sum_pv_wet test data

---
+++ tests/data/sum_pv_wet.conflux.py
@@ -0,0 +1,18 @@
+import xarray as xr
+
+product_name = 'sum_pv_wet'
+version = '0.0.1'
+
+input_products = {
+    'wofs_albers': ['water'],
+    'ls7_fc_albers': ['PV'],
+}
+
+def transform(inputs: xr.Dataset) -> xr.Dataset:
+    return xr.Dataset({
+        'water': inputs.water == 128,
+        'pv': inputs.PV > 0.5,
+    })
+
+def summarise(inputs: xr.Dataset) -> xr.Dataset:
+    return inputs.sum()
