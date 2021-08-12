7bb33e882978dc2595d1ffd3db9cad3a640f0ca3
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-08-12 12:29:24 +1000

    Removed constants :(

--- tests/test_io.py
+++ tests/test_io.py
@@ -9,9 +9,21 @@ import datacube
 import pytest
 
 import dea_conflux.io as io
-from .constants import *
 
 logging.basicConfig(level=logging.INFO)
+
+# Test directory.
+HERE = Path(__file__).parent.resolve()
+
+# Path to Canberra test shapefile.
+TEST_SHP = HERE / 'data' / 'waterbodies_canberra.shp'
+
+TEST_PLUGIN_OK = HERE / 'data' / 'sum_wet.conflux.py'
+TEST_PLUGIN_COMBINED = HERE / 'data' / 'sum_pv_wet.conflux.py'
+TEST_PLUGIN_MISSING_TRANSFORM = HERE / 'data' / 'sum_wet_missing_transform.conflux.py'
+
+TEST_WOFL_ID = '234fec8f-1de7-488a-a115-818ebd4bfec4'
+TEST_FC_ID = '4d243358-152e-404c-bb65-7ea64b21ca38'
 
 
 def setup_module(module):
