26a0639ae78a6695d83ed1edef41619e7e4ef150
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-08-12 11:21:43 +1000

    helps to include the module

---
+++ tests/test_io.py
@@ -0,0 +1,18 @@
+import logging
+from pathlib import Path
+import re
+import sys
+
+from click.testing import CliRunner
+import datacube
+import pytest
+
+import dea_conflux.io as io
+from constants import *
+
+logging.basicConfig(level=logging.INFO)
+
+
+def setup_module(module):
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    logging.getLogger("").handlers = []
