eaa1778f79493e77ea50967f9f19fd4387e03f73
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-08-12 11:18:51 +1000

    Added constant test module

--- tests/test_drill.py
+++ tests/test_drill.py
@@ -9,21 +9,7 @@ import pytest
 
 from dea_conflux.__main__ import run_plugin
 from dea_conflux.drill import find_datasets, drill
-
-# Test directory.
-HERE = Path(__file__).parent.resolve()
-logging.basicConfig(level=logging.INFO)
-
-# Path to Canberra test shapefile.
-TEST_SHP = HERE / 'data' / 'waterbodies_canberra.shp'
-
-TEST_PLUGIN_OK = HERE / 'data' / 'sum_wet.conflux.py'
-TEST_PLUGIN_COMBINED = HERE / 'data' / 'sum_pv_wet.conflux.py'
-TEST_PLUGIN_MISSING_TRANSFORM = HERE / 'data' / 'sum_wet_missing_transform.conflux.py'
-
-TEST_WOFL_ID = '234fec8f-1de7-488a-a115-818ebd4bfec4'
-TEST_FC_ID = '4d243358-152e-404c-bb65-7ea64b21ca38'
-
+from constants import *
 
 def setup_module(module):
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
