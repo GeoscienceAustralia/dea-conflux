6cd041fc2dafa06e0722f9d316b103ad60dfab32
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-08-13 15:27:06 +1000

    Fixed import

--- dea_conflux/hopper.py
+++ dea_conflux/hopper.py
@@ -41,7 +41,7 @@ def find_datasets(
     Generator of datasets
     """
     if dc is None:
-        dc = datacube.Datacube()
+        dc = Datacube()
 
     # Find many datasets across many products with a limit
     count = 0
