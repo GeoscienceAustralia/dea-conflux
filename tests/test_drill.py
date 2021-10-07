75102df4e0f920591cbe28bf208dbe0bdad6b8ba
Bex Dunn <Bex.Dunn@ga.gov.au>
2021-10-08 07:48:58 +1100

    messed with test_get_directions to test the id and north overedge polygon used in test_north_overedge

--- tests/test_drill.py
+++ tests/test_drill.py
@@ -82,7 +82,7 @@ def test_drill_integration(dc):
 
 def test_get_directions(dc):
     gdf = gpd.read_file(TEST_NORTH_OVERLAP)
-    extent = dc.index.datasets.get(TEST_WOFL_ID).extent.geom
+    extent = dc.index.datasets.get(TEST_C3_WO_NTH_ID).extent.geom
     intersection = gdf.geometry.intersection(extent)
     dirs = _get_directions(gdf.geometry[0], intersection.geometry[0])
     assert dirs == {'North'}
