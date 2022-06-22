fe40076b116564307b0d4b33dbe118238d466898
Sai Ma <Sai.Ma@ga.gov.au>
2022-06-22 06:48:26 +0000

    Add mock test on sqs and run-one

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import pytest
 from click.testing import CliRunner
+from moto import mock_sqs
 
 import dea_conflux.__main__ as main_module
 from dea_conflux.__main__ import main
@@ -98,6 +99,58 @@ def test_validate_plugin_no_transform():
     plugin = main_module.run_plugin(TEST_PLUGIN_MISSING_TRANSFORM)
     with pytest.raises(ValueError):
         main_module.validate_plugin(plugin)
+
+
+def test_run_one(run_main):
+    run_one_result = run_main(
+        ["run-one", "-p", TEST_PLUGIN_OK, "-s", TEST_SHP, "-o", "test_output", "-vv"],
+        expect_success=True,
+    )
+    print(run_one_result)
+
+
+@mock_sqs
+def test_make_s3_queue(run_main):
+    make_queue_result = run_main(
+        ["make", "waterbodies_queue_name"], expect_success=True
+    )
+    print(make_queue_result)
+
+
+@mock_sqs
+def test_push_to_s3_queue(run_main):
+
+    import os
+
+    import boto3
+
+    queue_name = "waterbodies_queue_name"
+    file_name = "test_id.txt"
+
+    sqs = boto3.resource("sqs")
+    _ = sqs.create_queue(QueueName=queue_name)
+
+    with open(file_name, "w") as f:
+        f.write("")
+
+    make_queue_result = run_main(
+        ["push-to-queue", "--txt", file_name, "--queue", queue_name],
+        expect_success=True,
+    )
+    print(make_queue_result)
+    os.remove(file_name)
+
+
+@mock_sqs
+def test_delete_s3_queue(run_main):
+    queue_name = "waterbodies_queue_name"
+    import boto3
+
+    sqs = boto3.resource("sqs")
+    _ = sqs.create_queue(QueueName=queue_name)
+    _ = sqs.create_queue(QueueName=queue_name + "_deadletter")
+    del_queue_result = run_main(["delete", queue_name], expect_success=True)
+    print(del_queue_result)
 
 
 # TODO(MatthewJA): Add a test on scene 234fec8f-1de7-488a-a115-818ebd4bfec4.
