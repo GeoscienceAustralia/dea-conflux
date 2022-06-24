6b9ea7c093a2259274049dee2cde81c8f5fe0e3f
Sai Ma <Sai.Ma@ga.gov.au>
2022-06-24 05:50:11 +0000

    Add unit test to queue module

---
+++ tests/test_queue.py
@@ -0,0 +1,30 @@
+import pytest
+from moto import mock_sqs
+from dea_conflux.queues import get_queue, verify_name, move_to_deadletter_queue
+from click import ClickException
+
+# Under: s3://dea-public-data/baseline/ga_ls7e_ard_3/090/084/2000/02/02/*.json
+ARD_UUID = "b17ad657-00fa-4abe-91a6-07fd24895e5d"
+
+@mock_sqs
+def test_get_queue():
+    import boto3
+
+    sqs = boto3.resource("sqs")
+    _ = sqs.create_queue(QueueName="waterbodies_queue_name")
+
+    _ = get_queue("waterbodies_queue_name")
+
+def test_verify_name():
+    with pytest.raises(ClickException) as e_info:
+        verify_name("another_queue_name")
+    assert "DEA conflux queues must start with waterbodies_ or wit_" in str(e_info)
+
+
+@mock_sqs
+def test_move_to_deadletter_queue():
+    import boto3
+
+    sqs = boto3.resource("sqs")
+    _ = sqs.create_queue(QueueName="waterbodies_queue_dl")
+    _ = move_to_deadletter_queue("waterbodies_queue_dl", ARD_UUID)
