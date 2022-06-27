1918fbb73afeef98dbb27efbac46f0bac0d609e5
Sai Ma <Sai.Ma@ga.gov.au>
2022-06-27 08:11:05 +0000

    Change back to pytest-cov solution

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         install_requires=[
             "click",
             "pytest",
-            "coverage",
+            "pytest-cov",
             "geopandas",
             "datacube",
             "boto3",
