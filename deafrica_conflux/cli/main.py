7c5296265f052d6c44dcd31ed27ff1b38484dfb9
vikineema <victoria@kartoza.com>
2023-08-29 13:42:53 +0300

    Add get-ids command

--- deafrica_conflux/cli/main.py
+++ deafrica_conflux/cli/main.py
@@ -2,8 +2,13 @@ import click
 
 import deafrica_conflux.__version__
 
+from .get_ids import get_ids
+
 
 @click.version_option(package_name="deafrica_conflux", version=deafrica_conflux.__version__)
 @click.group(help="Run deafrica-conflux.")
 def main():
     pass
+
+
+main.add_command(get_ids)
