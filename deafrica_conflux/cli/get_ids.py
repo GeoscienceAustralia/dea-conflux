2355054fc0f2c51ba5f3e3f239192613c54dcfd9
vikineema <victoria@kartoza.com>
2023-08-30 19:03:41 +0300

    Update get-ids command to use updated drill module functions

--- deafrica_conflux/cli/get_ids.py
+++ deafrica_conflux/cli/get_ids.py
@@ -4,28 +4,27 @@ import click
 import logging
 import fsspec
 import json
-import uuid as pyuuid
+import geopandas as gpd
 from datacube.ui import click as ui
 
-from .common import main, command_required_option_from_option, logging_setup, get_crs, guess_id_field, load_and_reproject_shapefile
+from deafrica_conflux.cli.logs import logging_setup
+from deafrica_conflux.cli.group_options import MutuallyExclusiveOption
 
-import deafrica_conflux.hopper
-
-required_options = {
-    True: 'bucket_name',
-    False: 'output_folder',
-}
+from deafrica_conflux.id_field import guess_id_field
+from deafrica_conflux.hopper import find_datasets
+from deafrica_conflux.drill import filter_datasets
 
 
-@main.command("get-ids", no_args_is_help=True, cls=command_required_option_from_option('s3', required_options))
+@click.command("get-ids",
+               no_args_is_help=True,
+               )
 @click.argument("product", type=str)
 @ui.parsed_search_expressions
 @click.option("-v", "--verbose", count=True)
 @click.option(
-    "--shapefile",
-    "-s",
+    "--polygons-vector-file",
     type=click.Path(),
-    help="Path to the polygon " "shapefile to run polygon drill on to filter datasets.",
+    help="Path to the vector file defining the polygon(s) to run polygon drill on to filter datasets.",
 )
 @click.option(
     "--use-id",
@@ -34,40 +33,47 @@ required_options = {
     default=None,
     help="Optional. Unique key id in shapefile.",
 )
-@click.option("--s3/--local", default=False)
-@click.option(
-    "--bucket-name",
-    type=str,
-    help="The default s3 bucket to save the get_ids result.",
-    default="deafrica-waterbodies-dev",
-    show_default=True,
-    required=False,
-    is_eager=True
-)
-@click.option(
-    "--output-folder",
-    type=str,
-    help="The default folder to save the get_ids result. Default is the current directory.",
-    default="",
-    show_default=True,
-    required=False,
-    is_eager=True
-)
+@click.option("--s3",
+              "storage_location",
+              flag_value="s3",
+              help="Save the output to an s3 bucket.")
+@click.option("--local",
+              "storage_location",
+              flag_value="local",
+              default=True,
+              help="Save the output to a local folder.")
+@click.option("--output-bucket-name",
+              type=str,
+              show_default=True,
+              cls=MutuallyExclusiveOption,
+              mutually_exclusive=["output_local_folder"],
+              help="The s3 bucket to write the output to.",)
+@click.option("--output-local-folder",
+              type=click.Path(),
+              cls=MutuallyExclusiveOption,
+              mutually_exclusive=["output_bucket_name"],
+              help="Local directory to write the waterbody polygons to.",)
 @click.option(
     "--num-worker",
     type=int,
     help="The number of processes to filter datasets.",
     default=4,
 )
+@click.option("--product-version",
+              type=str,
+              default="0.0.1",
+              show_default=True,
+              help="Product version for the DE Africa Waterbodies product.")
 def get_ids(product,
             expressions,
             verbose,
-            shapefile,
+            polygons_vector_file,
             use_id,
-            s3,
-            bucket_name,
-            output_folder,
-            num_worker):
+            storage_location,
+            output_bucket_name,
+            output_local_folder,
+            num_worker,
+            product_version):
     
     """
     Get IDs based on an expression.
@@ -76,48 +82,43 @@ def get_ids(product,
     logging_setup(verbose)
     _log = logging.getLogger(__name__)
 
-    dss = deafrica_conflux.hopper.find_datasets(expressions, [product])
+    dss = find_datasets(expressions, [product])
 
-    if shapefile:
-        crs = get_crs(shapefile)
+    if polygons_vector_file:
 
+        # Read the vector file.
+        try:
+            polygons_gdf = gpd.read_file(polygons_vector_file)
+        except Exception as error:
+            _log.error(error)
+            raise
+    
         # Guess the ID field.
-        id_field = guess_id_field(shapefile, use_id)
+        id_field = guess_id_field(polygons_gdf, use_id)
         _log.debug(f"Guessed ID field: {id_field}")
 
-        # Load and reproject the shapefile.
-        shapefile = load_and_reproject_shapefile(
-            shapefile,
-            id_field,
-            crs,
-        )
-        _log.info(f"shapefile RAM usage: {sys.getsizeof(shapefile)}.")
+        # Set the ID field as the index.
+        polygons_gdf.set_index(id_field)
 
-        ids = deafrica_conflux.drill.filter_dataset(dss, shapefile, worker_num=num_worker)
+        _log.info(f"Polygons vector file RAM usage: {sys.getsizeof(polygons_gdf)} bytes.")
+
+        ids = filter_datasets(dss, polygons_gdf, worker_num=num_worker)
     else:
         ids = [str(ds.id) for ds in dss]
 
-    if not s3:
-        output_directory_suffix = "timeseries/conflux/"
-        output_directory = os.path.join(os.path.abspath(output_folder), output_directory_suffix)
+    object_prefix = f'{product_version.replace(".", "-")}/timeseries/conflux/'
+
+    if storage_location == "local":
+        output_directory = os.path.join(os.path.abspath(output_local_folder), object_prefix)
         
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
+            _log.info(f"{output_directory} folder created.")
     
-        out_path = (
-            f"{output_directory}"
-            + "conflux_ids_"
-            + str(pyuuid.uuid4())
-            + ".json"
-        )
+        out_path = f"file://{output_directory}conflux_ids.json"
     else:
-        out_path = (
-            f"s3://{bucket_name}/timeseries/conflux/"
-            + "conflux_ids_"
-            + str(pyuuid.uuid4())
-            + ".json"
-        )
-        
+        out_path = f"s3://{output_bucket_name}/{object_prefix}conflux_ids.json"
+
     _log.info(f"Writing IDs to: {out_path}.")
     
     with fsspec.open(out_path, "w") as f:
