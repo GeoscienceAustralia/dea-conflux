05a3bbf0fd198bb0975177192799c85e46c95801
vikineema <victoria@kartoza.com>
2023-09-15 22:24:00 +0300

    Update index tiles script

--- index_tiles.sh
+++ index_tiles.sh
@@ -1,5 +1,6 @@
-# Index tiles covering the waterbody UID: edumesbb2
-# wofs_ls
+#!/bin/bash
+
+# Index wofs_ls tiles covering the waterbody UID: edumesbb2
 s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/204/049/2023/01/07/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
 s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/205/048/2023/01/06/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
 s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/205/049/2023/01/06/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
