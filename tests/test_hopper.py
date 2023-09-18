3dd5cf260d503265c9272b9b09a62c9fc7f9941b
vikineema <victoria@kartoza.com>
2023-09-19 00:32:06 +0300

    Update hopper tests

--- tests/test_hopper.py
+++ tests/test_hopper.py
@@ -1,6 +1,5 @@
 import datacube
 import pytest
-from datacube.ui.click import parse_expressions
 
 from deafrica_conflux.hopper import find_datasets
 
@@ -11,11 +10,12 @@ def dc():
 
 
 def test_find_datasets(dc):
-    query = parse_expressions("lon in [-10.002, -7.929] lat in [13.408, 15.496] time in 2023-06-12")
-    datasets = find_datasets(query=query, products=["wofs_ls"], dc=dc)
-    assert len(list(datasets)) == 3
+    datasets = find_datasets(query={}, products=["wofs_ls"], dc=dc)
+    dss = list(datasets)
+    assert len(dss) == 24
 
 
 def test_find_no_datasets(dc):
     datasets = find_datasets(query={}, products=["not_exist_product"], dc=dc)
-    assert len(list(datasets)) == 0
+    dss = list(datasets)
+    assert len(dss) == 0
