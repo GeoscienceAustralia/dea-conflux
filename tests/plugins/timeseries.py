53ca92f94524d44d1561e97fbca388511ae58598
vikineema <victoria@kartoza.com>
2023-09-19 00:56:00 +0300

    Add tests for the deafrica-conflux tools

--- tests/plugins/timeseries.py
+++ tests/plugins/timeseries.py
@@ -1,10 +1,11 @@
 import numpy as np
 import xarray as xr
 
-product_name = "sum_wet"
+product_name = "waterbodies_timeseries"
 version = "0.0.1"
 resolution = (-30, 30)
 output_crs = "EPSG:6933"
+resampling = "nearest"
 
 input_products = {
     "wofs_ls": ["water"],
@@ -25,10 +26,24 @@ def transform(inputs: xr.Dataset) -> xr.Dataset:
 
 
 def summarise(inputs: xr.Dataset) -> xr.Dataset:
+    pixel_count = inputs.water.size
+
+    valid_count = np.count_nonzero(~np.isnan(inputs.water))
+    invalid_count = np.count_nonzero(np.isnan(inputs.water))
+
+    assert valid_count + invalid_count == pixel_count
+
+    valid_and_dry_count = np.count_nonzero(inputs.water == 0)
     valid_and_wet_count = np.count_nonzero(inputs.water == 128)
+
+    valid_and_wet_percentage = (valid_and_wet_count / pixel_count) * 100
+    valid_and_dry_percentage = (valid_and_dry_count / pixel_count) * 100  # noqa F841
+    invalid_percentage = (invalid_count / pixel_count) * 100
 
     return xr.Dataset(
         {
+            "wet_percentage": valid_and_wet_percentage,
             "wet_pixel_count": valid_and_wet_count,
+            "invalid_percentage": invalid_percentage,
         }
     )
---
+++ tests/test_id_field.py
@@ -0,0 +1,1 @@
+# TODO: Add tests for the id_field.py module
