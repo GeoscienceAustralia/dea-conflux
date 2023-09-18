3f0e2c7c60ee6ff02e555497b504fed3a790b473
vikineema <victoria@kartoza.com>
2023-09-19 00:31:36 +0300

    Update tiles for testing

--- index_tiles.sh
+++ index_tiles.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Add the wofs_ls datasets covering the waterbody UID: edumesbb2
+s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/204/048/2023/01/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
 s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/204/049/2023/01/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
+
 s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/205/048/2023/01/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
 s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/205/049/2023/01/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
+
 s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/206/048/2023/01/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
 s3-to-dc "s3://deafrica-services/wofs_ls/1-0-0/206/049/2023/01/*/*.json" --stac --no-sign-request --skip-lineage 'wofs_ls'
