e9cd28ca96296145e913b266fbcf9c5017933347
vikineema <victoria@kartoza.com>
2023-09-20 20:55:13 +0300

    Revert "Fix send batch with retry"
    This reverts commit a8f6d76b70bee441768de5c069dc87c79a11a225.

--- deafrica_conflux/queues.py
+++ deafrica_conflux/queues.py
@@ -354,18 +354,18 @@ def send_batch_with_retry(
     assert len(messages) <= 10
 
     retries = 0
-    sucessful_msg_list = []
+    sucessful = []
     while retries <= max_retries:
         sucessful_msgs, messages = send_batch(
             queue_url=queue_url, messages=messages, sqs_client=sqs_client
         )
-        sucessful_msg_list.extend[sucessful_msgs]
+        sucessful.extend[sucessful_msgs]
         if messages is None:
             break
         else:
             retries += 1
 
-    return sucessful_msg_list, messages
+    return sucessful, messages
 
 
 def send_batch(
