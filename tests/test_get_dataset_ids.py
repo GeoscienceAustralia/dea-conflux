4648ffba54a8dad4531f4993e095696d6d8cc437
vikineema <victoria@kartoza.com>
2023-09-20 17:45:49 +0300

    minor update

--- tests/test_get_dataset_ids.py
+++ tests/test_get_dataset_ids.py
@@ -27,15 +27,7 @@ def test_get_dataset_ids_cli():
         dataset_ids = f.readlines()
         dataset_ids = [idx.strip() for idx in dataset_ids]
 
-    expected_dataset_ids = [
-        "424bae19-35ac-5174-a40a-743450f86727",
-        "679bd9b7-f88b-5996-bb39-43a87cbe916a",
-        "3a736326-bb16-5112-a38e-8861b448cc8c",
-        "d15407ff-3fe5-55ec-a713-d4cc9399e6b3",
-        "c2ca91b7-1f55-5662-9e5d-3b1396af10f1",
-    ]
-
-    assert dataset_ids == expected_dataset_ids
+    assert len(dataset_ids) == 5
 
 
 def test_get_dataset_ids_with_existing_ids_file():
