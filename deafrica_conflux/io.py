d141c3fd57194be22dc41e011e1fd326d588bebe
vikineema <victoria@kartoza.com>
2023-09-22 17:50:36 +0300

    Simplify checking functions in io module

--- deafrica_conflux/io.py
+++ deafrica_conflux/io.py
@@ -10,14 +10,10 @@ import logging
 import os
 import urllib
 
-import boto3
 import fsspec
 import pandas as pd
 import pyarrow
 import pyarrow.parquet
-import s3urls
-from botocore.exceptions import ClientError
-from mypy_boto3_s3.client import S3Client
 
 _log = logging.getLogger(__name__)
 
@@ -119,18 +115,11 @@ def check_if_s3_uri(file_path: str) -> bool:
     Returns
     -------
     bool
-        True if the file path is an S3 URI or Object URL.
-        False if the file path is a local file path or File URI.
+        True if the file path is an S3 URI.
     """
-
     file_scheme = urllib.parse.urlparse(file_path).scheme
 
-    # Assumption here is urls with the scheme https and http
-    # are s3 file paths.
-
-    # All others are assumed to be local files.
-
-    valid_s3_schemes = ["s3", "http", "https"]
+    valid_s3_schemes = ["s3"]
 
     if file_scheme in valid_s3_schemes:
         return True
@@ -165,19 +154,17 @@ def table_exists(
     -------
     bool
     """
-    output_directory = str(output_directory)  # Support pathlib paths.
-
     if check_if_s3_uri(output_directory):
-        file_system = fsspec.filesystem("s3")
+        fs = fsspec.filesystem("s3")
     else:
-        file_system = fsspec.filesystem("file")
+        fs = fsspec.filesystem("file")
 
     file_name = make_parquet_file_name(drill_name, uuid, centre_date)
     folder_name = date_to_string_day(centre_date)
 
     path = os.path.join(output_directory, folder_name, file_name)
 
-    return file_system.exists(path)
+    return fs.exists(path)
 
 
 def write_table_to_parquet(
@@ -236,13 +223,16 @@ def write_table_to_parquet(
     table_pa = table_pa.replace_schema_metadata(combined_meta)
 
     # Write the table.
-    output_directory = str(output_directory)  # support pathlib paths
     folder_name = date_to_string_day(centre_date)
     file_name = make_parquet_file_name(drill_name, uuid, centre_date)
-    output_file_path = os.path.join(output_directory, folder_name, file_name)
 
-    if not check_if_s3_uri(output_directory):
-        os.makedirs(os.path.join(output_directory, folder_name), exist_ok=True)
+    if check_if_s3_uri(output_directory):
+        output_file_path = os.path.join(output_directory, folder_name, file_name)
+    else:
+        parent_folder = os.path.join(output_directory, folder_name)
+        fs = fsspec.filesystem("file")
+        fs.makedirs(parent_folder, exist_ok=True)
+        output_file_path = os.path.join(parent_folder, file_name)
 
     pyarrow.parquet.write_table(table_pa, output_file_path, compression="GZIP")
 
@@ -272,7 +262,7 @@ def read_table_from_parquet(path: str) -> pd.DataFrame:
     return df
 
 
-def check_local_dir_exists(dir_path: str):
+def check_dir_exists(dir_path: str):
     """
     Checks if a specified path is an existing directory.
 
@@ -287,8 +277,13 @@ def check_local_dir_exists(dir_path: str):
         True if the path exists and is a directory.
         False if the path does not exists or if the path exists and it is not a directory.
     """
-    if os.path.exists(dir_path):
-        if os.path.isdir(dir_path):
+    if check_if_s3_uri(dir_path):
+        fs = fsspec.filesystem("s3")
+    else:
+        fs = fsspec.filesystem("file")
+
+    if fs.exists(dir_path):
+        if fs.isdir(dir_path):
             return True
         else:
             return False
@@ -296,7 +291,7 @@ def check_local_dir_exists(dir_path: str):
         return False
 
 
-def check_local_file_exists(file_path: str) -> bool:
+def check_file_exists(file_path: str) -> bool:
     """
     Checks if a specified path is an existing file.
 
@@ -305,70 +300,15 @@ def check_local_file_exists(file_path: str) -> bool:
     file_path : str
         Path to check.
     """
-    if os.path.exists(file_path):
-        if os.path.isfile(file_path):
+    if check_if_s3_uri(file_path):
+        fs = fsspec.filesystem("s3")
+    else:
+        fs = fsspec.filesystem("file")
+
+    if fs.exists(file_path):
+        if fs.isfile(file_path):
             return True
         else:
             return False
-    else:
-        return False
-
-
-def check_s3_bucket_exists(bucket_name: str, s3_client: S3Client = None) -> bool:
-    """
-    Check if a bucket exists and if the user has permission to access it.
-
-    Parameters
-    ----------
-    bucket_name : str
-        Name of s3 bucket to check.
-    s3_client : S3Client
-        A low-level client representing Amazon Simple Storage Service (S3), by default None.
-
-    bool:
-        True if the bucket exists and the user has access to the bucket.
-        False if the bucket does not exist or if the bucket exists but the user does not have access to the bucket.
-    """
-    # Get the service client.
-    if s3_client is None:
-        s3_client = boto3.client("s3")
-
-    try:
-        response = s3_client.head_bucket(Bucket=bucket_name)  # noqa E501
-    except ClientError:
-        return False
-    else:
-        return True
-
-
-def check_s3_object_exists(s3_object_uri: str, s3_client: S3Client = None):
-    """
-    Check if an object in an S3 bucket exists.
-
-    Parameters
-    ----------
-    s3_object_uri : str
-        S3 URI of the object to check.
-    s3_client : S3Client
-        A low-level client representing Amazon Simple Storage Service (S3), by default None.
-    bool:
-        True if the object exists and the user has access to the object.
-        False if the object does not exist or if the object exists but the user does not have access to the object.
-    """
-    # Get the service client.
-    if s3_client is None:
-        s3_client = boto3.client("s3")
-
-    bucket_name = s3urls.parse_url(s3_object_uri)["bucket"]
-    object_key = s3urls.parse_url(s3_object_uri)["key"]
-
-    # First check if bucket exists.
-    if check_s3_bucket_exists(bucket_name, s3_client) is True:
-        try:
-            response = s3_client.head_object(Bucket=bucket_name, Key=object_key)  # noqa
-        except ClientError:
-            return False
-        else:
-            return True
     else:
         return False
