4789f7f5bbf42f7993d90ef9635529a6a63f592a
vikineema <victoria@kartoza.com>
2023-10-16 21:49:25 +0300

    Remove unused function

--- deafrica_conflux/cli/logs.py
+++ deafrica_conflux/cli/logs.py
@@ -27,35 +27,3 @@ def logging_setup(verbose: int = 1):
     # Suppress all (other than CRITICAL) errors for boto3
     # logging.getLogger('botocore').setLevel(logging.WARNING)
     # logging.getLogger('boto3').setLevel(logging.WARNING)
-
-
-# Keeping this here incase its needed.
-def setup_logging(verbose: int):
-    """
-    Set up logging.
-
-    Arguments
-    ---------
-    verbose : int
-        Verbosity level (0, 1, 2).
-    """
-    loggers = [
-        logging.getLogger(name)
-        for name in logging.root.manager.loggerDict
-        if not name.startswith("fiona")
-        and not name.startswith("sqlalchemy")
-        and not name.startswith("boto")
-    ]
-    # For compatibility with docker+pytest+click stack...
-    stdout_hdlr = logging.StreamHandler(sys.stdout)
-    for logger in loggers:
-        if verbose == 0:
-            logging.basicConfig(level=logging.WARNING)
-        elif verbose == 1:
-            logging.basicConfig(level=logging.INFO)
-        elif verbose == 2:
-            logging.basicConfig(level=logging.DEBUG)
-        else:
-            raise click.ClickException("Maximum verbosity is -vv")
-        logger.addHandler(stdout_hdlr)
-        logger.propagate = False
