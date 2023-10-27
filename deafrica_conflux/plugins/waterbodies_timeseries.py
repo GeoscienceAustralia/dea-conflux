16c6a7dabca9e6d9b033cc37fe3191d508578e44
vikineema <victoria@kartoza.com>
2023-10-27 23:38:11 +0300

    Update plugin

--- deafrica_conflux/plugins/waterbodies_timeseries.py
+++ deafrica_conflux/plugins/waterbodies_timeseries.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import xarray as xr
 
 product_name = "waterbodies"
@@ -43,51 +45,54 @@ def transform(inputs: xr.Dataset) -> xr.Dataset:
     return xr.Dataset({"water": wofl_masked})
 
 
-def summarise(inputs: xr.Dataset, resolution: tuple) -> xr.Dataset:
-    """
-    Input to this function is a dataset, where the .water array contains
-    pixel values for a single polygon
-    Values are as follows:
-        1 = water
-        0 = dry
-        null = invalid (not wet or dry)
-    """
+def summarise(region_mask, intensity_image):
+    masked_intensity_image = intensity_image[region_mask]
+
     # Area of one pixel in metres squared
     # Use absolute value to remove any negative sign from resolution tuple
     px_area = abs(resolution[0] * resolution[1])
 
-    # Start with pixel based calculations, then get areas and percentage
-    px_total = float(len(inputs.water))
-    px_invalid = inputs.water.isnull().sum()
+    dry_pixel_value = 0
+    wet_pixel_value = 1
+    invalid_pixel_value = -9999
+
+    unique_values, unique_value_counts = np.unique(masked_intensity_image, return_counts=True)
+    unique_values = np.where(np.isnan(unique_values), invalid_pixel_value, unique_values)
+    unique_values_and_counts = dict(zip(unique_values, unique_value_counts))
+
+    # Start with pixel based calculations.
+    px_total = np.sum(unique_value_counts)
+    px_invalid = unique_values_and_counts.get(invalid_pixel_value, np.nan)
+    px_dry = unique_values_and_counts.get(dry_pixel_value, np.nan)
+    px_wet = unique_values_and_counts.get(wet_pixel_value, np.nan)
+
+    # Calculate areas.
+    area_invalid = px_invalid * px_area
+    area_dry = px_dry * px_area
+    area_wet = px_wet * px_area
+
+    # Calculate percentages.
+    pc_wet = (px_wet / px_total) * 100.0
+    pc_dry = (px_dry / px_total) * 100.0
     pc_invalid = (px_invalid / px_total) * 100.0
-    ar_invalid = px_invalid * px_area
 
-    # Calculate wet and dry pixel numbers and areas
-    px_wet = inputs.water.sum()
-    ar_wet = px_wet * px_area
-    px_dry = px_total - px_invalid - px_wet
-    ar_dry = px_dry * px_area
+    # If the proportion of the waterbody missing is greater than 10%,
+    # set the values for pc_wet and pc_dry to nan.
+    if pc_invalid > 10.0:
+        pc_wet = np.nan
+        pc_dry = np.nan
 
-    # Set pc_wet and pc_dry values to nan, which will be used if insufficient pixels are observed
-    pc_wet = float("nan")
-    pc_dry = float("nan")
-
-    # If the proportion of the waterbody missing is less than 10%, calculate values for pc_wet and pc_dry
-    if pc_invalid <= 10.0:
-        pc_wet = (px_wet / px_total) * 100.0
-        pc_dry = 100.0 - pc_invalid - pc_wet
-
-    # Return all calculated values
-    return xr.Dataset(
-        {
-            "pc_wet": pc_wet,
-            "px_wet": px_wet,
-            "area_wet_m2": ar_wet,
-            "pc_dry": pc_dry,
-            "px_dry": px_dry,
-            "area_dry_m2": ar_dry,
-            "pc_invalid": pc_invalid,
-            "px_invalid": px_invalid,
-            "area_invalid_m2": ar_invalid,
-        }
-    )
+    # Return all calculated values as a DataFrame.
+    results = {
+        "pc_wet": [pc_wet],
+        "px_wet": [px_wet],
+        "area_wet_m2": [area_wet],
+        "pc_dry": [pc_dry],
+        "px_dry": [px_dry],
+        "area_dry_m2": [area_dry],
+        "pc_invalid": [pc_invalid],
+        "px_invalid": [px_invalid],
+        "area_invalid_m2": [area_invalid],
+    }
+    results_df = pd.DataFrame(results)
+    return results_df
