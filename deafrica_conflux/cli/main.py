71f8b05fc36624944bae87a653c5769286f9b922
vikineema <victoria@kartoza.com>
2023-10-27 23:16:16 +0300

    Remove run-from-list, run-from-text instead

--- deafrica_conflux/cli/main.py
+++ deafrica_conflux/cli/main.py
@@ -7,7 +7,6 @@ from deafrica_conflux.cli.filter_from_queue import filter_from_queue
 from deafrica_conflux.cli.get_dataset_ids import get_dataset_ids
 from deafrica_conflux.cli.make_queue import make_sqs_queue
 from deafrica_conflux.cli.push_to_queue import push_to_sqs_queue
-from deafrica_conflux.cli.run_from_list import run_from_list
 from deafrica_conflux.cli.run_from_queue import run_from_sqs_queue
 from deafrica_conflux.cli.run_from_txt import run_from_txt
 from deafrica_conflux.cli.split_polygons_by_region import split_polygons
@@ -22,7 +21,6 @@ def main():
 
 main.add_command(split_polygons)
 main.add_command(get_dataset_ids)
-main.add_command(run_from_list)
 main.add_command(run_from_txt)
 main.add_command(make_sqs_queue)
 main.add_command(push_to_sqs_queue)
