8f0b7e6652fb9bf79443f1264b0db90e44973b14
vikineema <victoria@kartoza.com>
2023-10-27 23:33:45 +0300

    Update run_from_queue

--- deafrica_conflux/cli/run_from_queue.py
+++ deafrica_conflux/cli/run_from_queue.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from importlib import import_module
 
 import boto3
@@ -17,7 +18,7 @@ from deafrica_conflux.queues import (
     delete_batch_with_retry,
     get_queue_url,
     move_to_dead_letter_queue,
-    receive_a_message,
+    receive_messages,
 )
 from deafrica_conflux.stack import stack_waterbodies_parquet_to_db
 
@@ -27,6 +28,7 @@ from deafrica_conflux.stack import stack_waterbodies_parquet_to_db
     no_args_is_help=True,
     help="Run deafrica-conflux on dataset ids from an SQS queue.",
 )
+@click.option("-v", "--verbose", count=True)
 @click.option(
     "--plugin-name",
     type=str,
@@ -38,22 +40,21 @@ from deafrica_conflux.stack import stack_waterbodies_parquet_to_db
     help="SQS Queue to read dataset IDs from to run deafrica-conflux on.",
 )
 @click.option(
-    "--polygons-vector-file",
-    type=click.Path(),
+    "--polygons-directory",
+    type=str,
     # Don't mandate existence since this might be s3://.
-    help="Path to the vector file defining the polygon(s) to run polygon drill on.",
+    help="Directory containing the parquet files for the polygons to perform the drill on \
+    split by product regions.",
 )
 @click.option(
     "--use-id",
-    "-u",
     type=str,
-    default=None,
+    default="",
     help="Optional. Unique key id in polygons vector file.",
 )
 @click.option(
     "--output-directory",
-    "-o",
-    type=click.Path(),
+    type=str,
     default=None,
     # Don't mandate existence since this might be s3://.
     help="REQUIRED. File URI or S3 URI to the output directory.",
@@ -73,7 +74,6 @@ from deafrica_conflux.stack import stack_waterbodies_parquet_to_db
     default=False,
     help="Rerun scenes that have already been processed.",
 )
-@click.option("-v", "--verbose", count=True)
 @click.option(
     "--visibility-timeout",
     default=18 * 60,
@@ -87,19 +87,19 @@ from deafrica_conflux.stack import stack_waterbodies_parquet_to_db
 @click.option("--db/--no-db", default=True, help="Write to the Waterbodies database.")
 @click.option(
     "--dump-empty-dataframe/--not-dump-empty-dataframe",
-    default=True,
+    default=False,
     help="Not matter DataFrame is empty or not, always as it as Parquet file.",
 )
 def run_from_sqs_queue(
+    verbose,
     plugin_name,
     dataset_ids_queue,
-    polygons_vector_file,
+    polygons_directory,
     use_id,
     output_directory,
     partial,
-    overwrite,
     overedge,
-    verbose,
+    overwrite,
     visibility_timeout,
     max_retries,
     db,
@@ -108,8 +108,13 @@ def run_from_sqs_queue(
     """
     Run deafrica-conflux on dataset ids from an SQS queue.
     """
+    # Set up logger.
     logging_setup(verbose)
     _log = logging.getLogger(__name__)
+
+    # "Support" pathlib Paths.
+    polygons_directory = str(polygons_directory)
+    output_directory = str(output_directory)
 
     # Read the plugin as a Python module.
     module = import_module(f"deafrica_conflux.plugins.{plugin_name}")
@@ -121,20 +126,6 @@ def run_from_sqs_queue(
     # Get the product name from the plugin.
     product_name = plugin.product_name
 
-    # Read the polygons vector file.
-    try:
-        polygons_gdf = gpd.read_file(polygons_vector_file)
-    except Exception as error:
-        _log.exception(f"Could not read file {polygons_vector_file}")
-        raise error
-    else:
-        # Guess the ID field.
-        id_field = guess_id_field(polygons_gdf, use_id)
-        _log.info(f"Guessed ID field: {id_field}")
-
-        # Set the ID field as the index.
-        polygons_gdf.set_index(id_field, inplace=True)
-
     # Create the service client.
     sqs_client = boto3.client("sqs")
 
@@ -143,6 +134,7 @@ def run_from_sqs_queue(
     dead_letter_queue_name = f"{dataset_ids_queue}-deadletter"
     dead_letter_queue_url = get_queue_url(queue_name=dead_letter_queue_name, sqs_client=sqs_client)
 
+    # Connect to the datacube.
     if db:
         engine = get_engine_waterbodies()
 
@@ -151,18 +143,21 @@ def run_from_sqs_queue(
     retries = 0
     while retries <= max_retries:
         # Retrieve a single message from the dataset_ids_queue.
-        message = receive_a_message(
+        retrieved_message = receive_messages(
             queue_url=dataset_ids_queue_url,
             max_retries=max_retries,
             visibility_timeout=visibility_timeout,
+            max_no_messages=1,
             sqs_client=sqs_client,
         )
-        if message is None:
+        if retrieved_message is None:
             retries += 1
         else:
             retries = 0  # reset the count
 
-            # Process the ID.
+            message = retrieved_message[0]
+
+            # Process the dataset ID.
             dataset_id = message["Body"]
             _log.info(f"Read dataset id {dataset_id} from queue {dataset_ids_queue_url}")
 
@@ -172,40 +167,62 @@ def run_from_sqs_queue(
 
             # Produce the parquet file.
             success_flag = True
-            centre_date = dc.index.datasets.get(dataset_id).center_time
 
+            # Load the dataset using the dataset id.
+            reference_dataset = dc.index.datasets.get(dataset_id)
+
+            # Get the region code for the dataset.
+            region_code = reference_dataset.metadata.region_code
+
+            # Get the center time for the dataset
+            centre_time = reference_dataset.center_time
+
+            # Use the center time to check if a parquet file for the dataset already
+            # exists in the output directory.
             if not overwrite:
                 _log.info(f"Checking existence of {dataset_id}")
-                exists = table_exists(
-                    drill_name=product_name,
-                    uuid=dataset_id,
-                    centre_date=centre_date,
-                    output_directory=output_directory,
-                )
+                exists = table_exists(product_name, dataset_id, centre_time, output_directory)
 
             if overwrite or not exists:
                 try:
+                    # Load the water body polygons for the region
+                    polygons_vector_file = os.path.join(
+                        polygons_directory, f"{region_code}.parquet"
+                    )
+                    try:
+                        polygons_gdf = gpd.read_parquet(polygons_vector_file)
+                    except Exception as error:
+                        _log.exception(f"Could not read file {polygons_vector_file}")
+                        raise error
+                    else:
+                        # Guess the ID field.
+                        id_field = guess_id_field(polygons_gdf, use_id)
+                        _log.debug(f"Guessed ID field: {id_field}")
+
+                        # Set the ID field as the index.
+                        polygons_gdf.set_index(id_field, inplace=True)
+
+                    # Perform the polygon drill on the dataset
                     table = drill(
                         plugin=plugin,
                         polygons_gdf=polygons_gdf,
-                        scene_uuid=dataset_id,
+                        reference_dataset=reference_dataset,
                         partial=partial,
                         overedge=overedge,
                         dc=dc,
                     )
 
-                    # if always dump drill result, or drill result is not empty,
-                    # dump that dataframe as PQ file
+                    # Write the table to a parquet file.
                     if (dump_empty_dataframe) or (not table.empty):
                         pq_filename = write_table_to_parquet(
                             drill_name=product_name,
                             uuid=dataset_id,
-                            centre_date=centre_date,
+                            centre_date=centre_time,
                             table=table,
                             output_directory=output_directory,
                         )
                         if db:
-                            _log.info(f"Writing {pq_filename} to DB")
+                            _log.info(f"Writing {pq_filename} to database")
                             stack_waterbodies_parquet_to_db(
                                 parquet_file_paths=[pq_filename],
                                 verbose=verbose,
