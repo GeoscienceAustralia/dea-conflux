043c656f969bfde043c5a1d8e53febeff0fce4b0
vikineema <victoria@kartoza.com>
2023-10-27 20:33:16 +0300

    Move split_polygons_by_regions function into separate module

--- deafrica_conflux/cli/split_polygons_by_region.py
+++ deafrica_conflux/cli/split_polygons_by_region.py
@@ -1,19 +1,15 @@
 import logging
-import os
 
 import click
-import fsspec
 import geopandas as gpd
-import numpy as np
-import pandas as pd
 
 from deafrica_conflux.cli.logs import logging_setup
+from deafrica_conflux.group_polygons import split_polygons_by_region
 from deafrica_conflux.id_field import guess_id_field
-from deafrica_conflux.io import check_dir_exists, check_if_s3_uri
 
 
 @click.command(
-    "split-polygons-by-region",
+    "split-polygons",
     no_args_is_help=True,
 )
 @click.option("-v", "--verbose", count=True)
@@ -35,7 +31,7 @@ from deafrica_conflux.io import check_dir_exists, check_if_s3_uri
     type=str,
     help="Path to the directory to write the parquet files to.",
 )
-def split_polygons_by_region(
+def split_polygons(
     verbose,
     product,
     polygons_vector_file,
@@ -60,48 +56,6 @@ def split_polygons_by_region(
         # Set the ID field as the index.
         polygons_gdf.set_index(id_field, inplace=True)
 
-    # Load the regions file.
-    product_regions_fp = f"https://explorer.digitalearth.africa/api/regions/{product}"
-    product_regions = gpd.read_file(product_regions_fp).to_crs(polygons_gdf.crs)
-    product_regions.set_index("region_code", inplace=True)
-
-    # Split each row in the product_regions into a GeoDataFrame of its own.
-    regions = np.array_split(product_regions, len(product_regions))
-    assert len(regions) == len(product_regions)
-
-    # For each region get the IDs for the polygons that intersect with the region.
-    def get_intersecting_polygons_ids(region, polygons_gdf):
-        intersecting_polygons_ids = gpd.sjoin(
-            polygons_gdf, region, how="inner", predicate="intersects"
-        ).index.to_list()
-        region["intersecting_polygons_ids"] = ",".join(intersecting_polygons_ids)
-        return region
-
-    regions_ = [get_intersecting_polygons_ids(region, polygons_gdf) for region in regions]
-
-    # Filter to remove regions with no intersecting polygons.
-    filtered_regions = [region for region in regions_ if region.iloc[0].intersecting_polygons_ids]
-
-    filtered_regions_gdf = pd.concat(filtered_regions, ignore_index=True)
-
-    if not check_dir_exists(output_directory):
-        if check_if_s3_uri(output_directory):
-            fs = fsspec.filesystem("s3")
-        else:
-            fs = fsspec.filesystem("file")
-
-        fs.mkdirs(output_directory, exist_ok=True)
-        _log.info(f"Created directory {output_directory}")
-
-    def export_polygons(row, polygons_gdf, output_directory):
-        region_id = row.name
-        polygon_ids = row.intersecting_polygons_ids.split(",")
-
-        output_fp = os.path.join(output_directory, f"{region_id}.parquet")
-        polygons_gdf.loc[polygon_ids].reset_index().to_parquet(output_fp)
-        _log.info(f"Polygons for region {region_id} written to {output_fp}")
-        return output_fp
-
-    filtered_regions_gdf["polygon_file_paths"] = filtered_regions_gdf.apply(
-        lambda row: export_polygons(row, polygons_gdf, output_directory), axis=1
+    split_polygons_fps = split_polygons_by_region(  # noqa F841
+        product=product, polygons_gdf=polygons_gdf, output_directory=output_directory
     )
