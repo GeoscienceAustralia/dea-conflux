d72b2be92ad3252e15d054d383a8fd9ac53f564f
vikineema <victoria@kartoza.com>
2023-10-27 14:52:41 +0300

    Add function to filter datasets based on region code

--- deafrica_conflux/hopper.py
+++ deafrica_conflux/hopper.py
@@ -62,3 +62,33 @@ def find_datasets(
                 f"Error searching for datasets. Maybe none were returned? Error was {error}"
             )
             continue
+
+
+def check_ds_region(region_codes: list, ds: Dataset) -> str:
+    """
+    Check if the region code of a dataset is in the list of region codes
+    provided. If True, returns the dataset's ID, if False, returns an empty
+    string.
+
+    Parameters
+    ----------
+    region_codes : list
+        A list of region codes to check the dataset's region code against.
+    ds : Dataset
+        The dataset to check.
+
+    Returns
+    -------
+    str
+        If the dataset's region code is in the list of region codes `region_codes`,
+        returns the dataset's ID.
+        If it is not, returns an empty string.
+    """
+    # Get the dataset's region code.
+    ds_region_code = ds.metadata.region_code
+    if ds_region_code in region_codes:
+        # Get the dataset id.
+        ds_id = str(ds.id)
+        return ds_id
+    else:
+        return ""
