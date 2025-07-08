7cea3794a7c5030a466c94156f7711afbc3b6a17
Abeer Mahendroo <abeer.mahendroo@ga.gov.au>
2025-07-08 04:16:26 +0000

    pylint flake8 fixes

--- dea_conflux/stack.py
+++ dea_conflux/stack.py
@@ -194,7 +194,7 @@ def remove_timeseries_with_duplicated(df: pd.DataFrame) -> pd.DataFrame:
     # The pc_missing the less the better, so we only keep the first one
     df = df.drop_duplicates("DAY", keep="first")
 
-    # Remove entries within 60s of the next one, this takes care of the edge case where duplicates wrap across midnight UTC
+    # Remove entries within 60s, this takes care of the edge case where duplicates wrap across midnight UTC
     df['TIMEDIFF'] = pd.to_datetime(df['date'].shift(-1)) - pd.to_datetime(df['date'])
     df = df[~(df['TIMEDIFF'] < timedelta(seconds=60))]
     df = df.drop(columns=["TIMEDIFF"])
