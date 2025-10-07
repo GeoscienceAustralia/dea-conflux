6ece65397339a76c2c522e90e3736d0bdad1f839
Abeer <abeer.mahendroo@ga.gov.au>
2025-10-07 17:14:03 +1100

    Update test_stack.py - logging fix

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -50,9 +50,6 @@ TEST_WOFL_ID = "234fec8f-1de7-488a-a115-818ebd4bfec4"
 TEST_FC_ID = "4d243358-152e-404c-bb65-7ea64b21ca38"
 
 
-def setup_module(module):
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, force=True)
-
 
 @pytest.fixture()
 def conflux_table():
